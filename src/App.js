ca7b09e38875b7f7943e97cf2835719672058d86
john-smilga <johnsmilga@gmail.com>
2019-11-27 07:55:44 -0800

    router imports

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// react router dom
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// pages
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+// compone
+export default function App() {
+  return <h1>hello from app component</h1>;
+}
